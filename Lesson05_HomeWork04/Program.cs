c5438fc6522ad42c95bd8b1d278af580800e7efc
Dima <Dima@RDN-PC>
2020-11-21 14:50:17 +0200

    HW4+HW5

---
+++ Lesson05_HomeWork04/Program.cs
@@ -0,0 +1,394 @@
+﻿using System;
+
+namespace Lesson05_HomeWork04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userAction;
+            bool correctOperator;
+            int tmpNum;
+
+            // Размеры матриц
+            uint matrix1_X;
+            uint matrix1_Y;
+            uint matrix2_X;
+            uint matrix2_Y;
+
+            int[,] matrix1;
+            int[,] matrix2;
+            int[,] matrixRez;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите действие над матрицами:");
+                Console.WriteLine("\t1 - Сложение матриц");
+                Console.WriteLine("\t2 - Умножение матрицы на число");
+                Console.WriteLine("\t3 - Умножение матриц");
+                Console.WriteLine("\t4 - Возведение матрицы в степень");
+                Console.WriteLine("\t5 - Транспонирование матриц");
+                Console.WriteLine("\t6 - Завершить работу");
+                userAction = Console.ReadLine();
+                correctOperator = true;
+
+                switch (userAction)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Складывать можно только матрицы одинакового размера!");
+
+                            // Размеры матриц
+                            matrix1_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+                            matrix1_Y = GetSizeMatrix(2);        // Кол-во столбцов матрицы
+
+                            // Создаем матрицы
+                            matrix1 = new int[matrix1_X, matrix1_Y];
+                            matrix2 = new int[matrix1_X, matrix1_Y];
+
+                            // Заполняем матрицы
+                            Console.WriteLine("Заполняем матрицу A");
+                            GetMatrix(ref matrix1);
+
+                            Console.WriteLine("Заполняем матрицу B");
+                            GetMatrix(ref matrix2);
+
+                            Console.WriteLine("Матрица A:");
+                            PrintMatrix(ref matrix1);
+                            Console.WriteLine("Матрица B:");
+                            PrintMatrix(ref matrix2);
+
+                            // Сложение матриц
+                            matrixRez = MatrixAddition(matrix1, matrix2);
+
+                            Console.WriteLine("Результат сложения матриц:");
+                            PrintMatrix(ref matrixRez);
+
+                            break;
+                        }
+                    case "2":
+                        {
+                            // Размеры матриц
+                            matrix1_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+                            matrix1_Y = GetSizeMatrix(2);        // Кол-во столбцов матрицы
+
+                            // Создаем матрицу
+                            matrix1 = new int[matrix1_X, matrix1_Y];
+
+                            // Заполняем матрицы
+                            Console.WriteLine("Заполняем матрицу");
+                            GetMatrix(ref matrix1);
+
+                            Console.WriteLine("Матрица 1:");
+                            PrintMatrix(ref matrix1);
+
+                            do
+                            {
+                                Console.WriteLine("Введите число для умножения матрицы:");
+                            }
+                            while (!int.TryParse(Console.ReadLine(), out tmpNum));
+
+                            // Умножение матрицы на число
+                            matrixRez = MatrixMult(matrix1, tmpNum);
+
+                            Console.WriteLine("Результат умножения матрицы на число:");
+                            PrintMatrix(ref matrixRez);
+
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Для умноженя матрицы должны быть совместимы!");
+                            Console.WriteLine("(число строк матрицы A равно числу столбцов матрицы B)");
+                            Console.WriteLine();
+
+                            // Размеры матриц
+                            Console.WriteLine("Матрица A:");
+                            matrix1_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+                            matrix1_Y = GetSizeMatrix(2);        // Кол-во столбцов матрицы
+                            Console.WriteLine("Матрица В:");
+                            matrix2_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+                            matrix2_Y = GetSizeMatrix(2);        // Кол-во столбцов матрицы
+
+                            if (matrix1_X != matrix2_Y && matrix1_Y != matrix2_X)
+                            {
+                                Console.WriteLine("Ошибка! Для умноженя матрицы должны быть совместимы!");
+                                Console.WriteLine("(число строк матрицы A равно числу столбцов матрицы B)");
+                                Console.ReadKey();
+                                break;
+                            }
+
+                            // Создаем матрицы
+                            matrix1 = new int[matrix1_X, matrix1_Y];
+                            matrix2 = new int[matrix2_X, matrix2_Y];
+
+                            // Заполняем матрицы
+                            Console.WriteLine();
+                            Console.WriteLine("Заполняем матрицу A");
+                            GetMatrix(ref matrix1);
+
+                            Console.WriteLine("Заполняем матрицу B");
+                            GetMatrix(ref matrix2);
+
+                            Console.WriteLine("Матрица A:");
+                            PrintMatrix(ref matrix1);
+                            Console.WriteLine("Матрица B:");
+                            PrintMatrix(ref matrix2);
+
+                            // Умножение матриц
+                            matrixRez = MatrixMult(matrix1, matrix2);
+
+                            Console.WriteLine();
+                            Console.WriteLine("Результат умножения матриц A*B:");
+                            PrintMatrix(ref matrixRez);
+
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("Допускается возводить в степень только квадратные матрицы!");
+                            Console.WriteLine("(число строк должно быть равно числу столбцов)");
+                            Console.WriteLine();
+
+                            // Размеры матриц
+                            Console.WriteLine("Матрица A:");
+                            matrix1_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+
+                            // Создаем матрицы
+                            matrix1 = new int[matrix1_X, matrix1_X];
+                            matrix2 = new int[matrix1_X, matrix1_X];
+
+                            // Заполняем матрицы
+                            Console.WriteLine();
+                            Console.WriteLine("Заполняем матрицу A");
+                            GetMatrix(ref matrix1);
+
+                            Console.WriteLine("Матрица A:");
+                            PrintMatrix(ref matrix1);
+
+                            do
+                            {
+                                Console.WriteLine("Введите степень матрицы:");
+                            }
+                            while (!int.TryParse(Console.ReadLine(), out tmpNum) || (tmpNum < 0));
+
+
+                            // Возведение матрицы в степень
+                            matrixRez = MatrixSqrt(matrix1, tmpNum);
+
+                            Console.WriteLine();
+                            Console.WriteLine("Результат возведения матрицы в степень:");
+                            PrintMatrix(ref matrixRez);
+
+                            break;
+                        }
+                    case "5":
+                        {
+                            // Размеры матриц
+                            matrix1_X = GetSizeMatrix(1);        // Кол-во строк матрицы
+                            matrix1_Y = GetSizeMatrix(2);        // Кол-во столбцов матрицы
+
+                            // Создаем матрицы
+                            matrix1 = new int[matrix1_X, matrix1_Y];
+
+                            // Заполняем матрицы
+                            Console.WriteLine("Заполняем матрицу");
+                            GetMatrix(ref matrix1);
+
+                            Console.WriteLine("Матрица A:");
+                            PrintMatrix(ref matrix1);
+
+                            // Транспонирование матриц
+                            matrixRez = MatrixTransp(matrix1);
+
+                            Console.WriteLine("Результат танспонирования матрицы:");
+                            PrintMatrix(ref matrixRez);
+
+                            break;
+                        }
+                    case "6":
+                        {
+                            correctOperator = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Неправильная операция!");
+                            break;
+                        }
+                }
+
+                if (userAction != "6")
+                {
+                    Console.WriteLine("Для продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+
+            }
+            while (correctOperator);
+
+        }
+
+
+        // Запрос размеров матриц
+        /// <param name="typeLine"></param>
+        /// 1 - строки, 2 - столбцы
+        static uint GetSizeMatrix(uint typeLine)
+        {
+            uint newCount;
+
+            do
+            {
+                Console.WriteLine("Введите количество " + (typeLine == 1 ? "строк":"столбцов") + " матрицы:");
+            }
+            while (!uint.TryParse(Console.ReadLine(), out newCount));
+
+            return newCount;
+        }
+
+        // Заполнение матриц
+        static void GetMatrix(ref int[,] matrix)
+        {
+            // Запрашиваем значения ячеек матрицы
+            for (uint i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = GetCellValue(i, j);
+                }
+            }
+        }
+
+        // Запрашиваем значения ячеек матрицы
+        static int GetCellValue(uint x, uint y)
+        {
+            int cellValue;
+          
+            do
+            {
+                Console.WriteLine("Введите значение ячейки [" + x + "," + y + "]");
+            }
+            while (!int.TryParse(Console.ReadLine(), out cellValue));
+
+            return cellValue;
+        }
+
+        // Вывод матрицы
+        static void PrintMatrix(ref int[,] matrix)
+        {
+            for (uint i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        // Сложение матриц
+        static int[,] MatrixAddition(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] matrix3 = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+            for (uint i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return matrix3;
+        }
+
+        // Умножение матрицы на число
+        static int[,] MatrixMult(int[,] matrix1, int tmpNum)
+        {
+            int[,] matrix3 = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+            for (uint i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] * tmpNum;
+                }
+            }
+
+            return matrix3;
+        }
+
+        // Умножение матриц
+        static int[,] MatrixMult(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            for (uint i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    int tmpVal = 0;
+
+                    for (uint k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                            tmpVal = tmpVal + matrix1[i, k] * matrix2[k, j];
+                    }
+
+                    matrix3[i, j] = tmpVal;
+
+                }
+            }
+
+            return matrix3;
+        }
+
+        // Возведение матрицы в степень
+        static int[,] MatrixSqrt(int[,] matrix1, int tmpNum)
+        {
+            int[,] matrix3 = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+            if (tmpNum == 0)
+            {
+                for (uint i = 0; i < matrix1.GetLength(0); i++)
+                {
+                    for (uint j = 0; j < matrix1.GetLength(1); j++)
+                    {
+                        matrix3[i,j] = (i==j) ? 1 : 0;
+                    }
+                }
+            }
+            else
+            {
+                matrix3 = matrix1;
+
+                for (uint i = 2; i <= tmpNum; i++)
+                {
+                    matrix3 = MatrixMult(matrix1, matrix3);
+                }
+
+            }
+
+            return matrix3;
+        }
+
+        // Транспонирование матриц
+        static int[,] MatrixTransp(int[,] matrix1)
+        {
+            int[,] matrix3 = new int[matrix1.GetLength(1), matrix1.GetLength(0)];
+
+            for (uint i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (uint j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix3[j, i] = matrix1[i, j];
+                }
+            }
+
+            return matrix3;
+        }
+
+    }
+}
---
+++ Lesson05_HomeWork05/Program.cs
@@ -0,0 +1,306 @@
+﻿using System;
+
+namespace Lesson05_HomeWork05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAction;
+            bool correctOperator;
+
+            uint sizeMas;
+            int[] arrayInp;
+            int[] arrayRez;
+
+            do
+            {
+                Console.WriteLine("Введите размер одномерного массива:");
+            }
+            while (!uint.TryParse(Console.ReadLine(), out sizeMas) || (sizeMas < 1));
+
+            // Создаем массив
+            arrayInp = new int[sizeMas];
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите метод сортировки:");
+                Console.WriteLine("\t1 - Сортировка пузырьком \t (Bubble sort)");
+                Console.WriteLine("\t2 - Быстрая сортировка \t\t (Quicksort)");
+                Console.WriteLine("\t3 - Гномья сортировка \t\t (GnomeSort)");
+                Console.WriteLine("\t4 - Сортировка Шелла \t\t (ShellSort)");
+                Console.WriteLine("\t5 - Завершить работу");
+                userAction = Console.ReadLine();
+                correctOperator = true;
+
+                switch (userAction)
+                {
+                    case "1":
+                        {
+                            // Заполняем массив
+                            InitArray(ref arrayInp);
+
+                            // Вывод неотсортированного массива
+                            Console.WriteLine("Неотсортированный массив:");
+                            PrintArray(arrayInp);
+
+                            // Сортировка - Bubble sort
+                            arrayRez = SortBubble(arrayInp);
+
+                            // Вывод отсортированного массива
+                            Console.WriteLine("Отсортированный массив:");
+                            PrintArray(arrayRez);
+
+                            break;
+                        }
+                    case "2":
+                        {
+                            // Заполняем массив
+                            InitArray(ref arrayInp);
+
+                            // Вывод неотсортированного массива
+                            Console.WriteLine("Неотсортированный массив:");
+                            PrintArray(arrayInp);
+
+                            // Сортировка - QuickSort
+                            arrayRez = QuickSort(arrayInp, 0, arrayInp.Length - 1);
+
+                            // Вывод отсортированного массива
+                            Console.WriteLine("Отсортированный массив:");
+                            PrintArray(arrayRez);
+
+                            break;
+                        }
+                    case "3":
+                        {
+                            // Заполняем массив
+                            InitArray(ref arrayInp);
+
+                            // Вывод неотсортированного массива
+                            Console.WriteLine("Неотсортированный массив:");
+                            PrintArray(arrayInp);
+
+                            // Сортировка - GnomeSort
+                            arrayRez = GnomeSort(arrayInp);
+
+                            // Вывод отсортированного массива
+                            Console.WriteLine("Отсортированный массив:");
+                            PrintArray(arrayRez);
+
+                            break;
+                        }
+                    case "4":
+                        {
+                            // Заполняем массив
+                            InitArray(ref arrayInp);
+
+                            // Вывод неотсортированного массива
+                            Console.WriteLine("Неотсортированный массив:");
+                            PrintArray(arrayInp);
+
+                            // Сортировка - ShellSort
+                            arrayRez = ShellSort(arrayInp);
+
+                            // Вывод отсортированного массива
+                            Console.WriteLine("Отсортированный массив:");
+                            PrintArray(arrayRez);
+
+                            break;
+                        }
+                    case "5":
+                        {
+                            correctOperator = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Неправильная операция!");
+                            break;
+                        }
+                }
+
+                if (userAction != "5")
+                {
+                    Console.WriteLine("Для продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+            }
+            while (correctOperator);
+        }
+
+
+        // // Заполняем массив
+        static void InitArray(ref int[] array)
+        {
+            Random random = new Random();
+
+            for (uint i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+        }
+
+        // Вывод массива
+        static void PrintArray(int[] array)
+        {
+            for (uint i = 0; i < array.GetLength(0); i++)
+            {
+                    Console.Write($"{array[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Обмен элементов массива
+        static void Swap(ref int item1, ref int item2)
+        {
+            int temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+
+        // Сортировка пузырьком
+        /// <summary>
+        /// За каждый проход элементы последовательно сравниваются попарно и, если порядок в паре неверный, 
+        /// выполняется обмен элементов.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static int[] SortBubble(int[] array)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        // Меняем местами элементы массива
+                        Swap(ref array[i + 1], ref array[i]);
+                    }
+                }
+            }
+
+            return array;
+        }
+
+
+        // Быстрая сортировка
+        /// <summary>
+        /// Выбирается опорный элемент, и массив делится на 2 подмассива: меньших опорного и больших опорного. 
+        /// Потом этот алгоритм применяется рекурсивно к подмассивам.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static int[] QuickSort(int[] array, int first, int last)
+        {
+            int left  = first;                          // левая граница
+            int right = last;                           // правая граница
+            int middle = array[(first + last) / 2];     // опорный элемент - средний
+
+            while (left <= right)                       // пока границы не сойдутся
+            {
+                while (array[left] < middle)            // сдвигаем левую границу пока элемент[left] меньше middle
+                    left++;
+                while (array[right] > middle)           // сдвигаем правую границу пока элемент[right] больше middle
+                    right--;
+
+                if (left <= right)                      // меняем элементы [left] и [right] местами 
+                {
+                    Swap(ref array[left], ref array[right]);
+                    left++;
+                    right--;
+                }
+            }
+
+            // Рекурсивно вызываем сортировку для левой и правой части массива
+            if (first < right)
+                array = QuickSort(array, first, right);
+            if (last > left)
+                array = QuickSort(array, left, last);
+
+            return array;
+        }
+
+
+        // Гномья сортировка
+        /// <summary>
+        /// За каждый проход элементы последовательно сравниваются попарно и, если порядок в паре неверный, 
+        /// выполняется обмен элементов.
+        /// 
+        /// Алгоритм находит первую пару элементов массива: если они в правильном порядке - идет дальше, 
+        /// если нет (неотсортированы) - меняет их местами и делает шаг назад.
+        /// Граничные условия: если нет предыдущего элемента массива, идем вперёд; если нет следующего - заканчиваем.
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static int[] GnomeSort(int[] array)
+        {
+            var indLeft= 1;
+            var indRight = indLeft + 1;
+
+            while (indLeft < array.Length)
+            {
+                // сравниваем пару элементов массива
+                if (array[indLeft - 1] < array[indLeft])        
+                {
+                    // пара элементов массива в правильном порядке - идем дальше
+                    indLeft = indRight;
+                    indRight++;
+                }
+                else
+                {
+                    // пара элементов массива неотсортирована - меняем их местами
+                    Swap(ref array[indLeft - 1], ref array[indLeft]);
+
+                    // и делаем шаг назад
+                    indLeft--;
+                    if (indLeft == 0)
+                    {
+                        indLeft = indRight;
+                        indRight++;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+
+        // Сортировка Шелла
+        /// <summary>
+        /// Сначала сравниваются и сортируются между собой значения, стоящие один от другого на некотором расстоянии step. 
+        /// После этого процедура повторяется для меньших значений step, 
+        /// и завершается сортировка Шелла упорядочиванием элементов при step=1.
+        /// 
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static int[] ShellSort(int[] array)
+        {
+            int j;
+            int step = array.Length / 2;
+
+            while (step > 0)
+            {
+                // Идем по элементам, которые сортируются на определённом шаге step
+                for (int i = 0; i < (array.Length - step); i++)
+                {
+                    j = i;
+                    while ((j >= 0) && (array[j] > array[j + step]))
+                    {
+                        // Меняем элементы местами
+                        Swap(ref array[j], ref array[j + step]);
+
+                        j -= step;
+                    }
+                }
+                step = step / 2;
+            }
+
+            return array;
+        }
+
+    }
+}
